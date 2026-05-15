602e63eebe94c94bba3b949f230d684101e24016
Nuel Young <nuelmega@gmail.com>
2026-05-15 23:12:39 +0100

    docs: update file-level docblocks to reference Tenantable
    Replace Nexus framework references with Tenantable in all source files

--- src/Commands/TenantsMakeModel.php
+++ src/Commands/TenantsMakeModel.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Tenantable.
+ *
+ * (c) Nuel Young Chukwunalu <nuelmega@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace nuelcyoung\tenantable\Commands;
